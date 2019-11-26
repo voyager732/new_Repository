70e24a5bdde437013f4f439555ae4a12b8a5d61c
p <58233566+voyager732@users.noreply.github.com>
2019-11-27 06:44:29 +0900

    projectTest

---
+++ gitTest/Source/gitTest/gitTestGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "gitTestGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AgitTestGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AgitTestGameMode();
+};
+
+
+
